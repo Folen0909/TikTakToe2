797020d4135a051735b2e4f33f68d5b761a6a8e2
Folen0909 <davidturkalj@gmail.com>
2019-06-17 21:49:24 +0200

    First commit

---
+++ src/main/java/com/turkalj/tiktaktoe/TiktaktoeApplication.java
@@ -0,0 +1,13 @@
+package com.turkalj.tiktaktoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiktaktoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiktaktoeApplication.class, args);
+    }
+
+}
